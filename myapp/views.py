c09085d1fb69cb99efa8f5f86ab91710d89d256a
wsolano <wsolano@ltechec.com>
2023-10-13 14:51:02 -0500

    Initial commit

---
+++ myapp/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def hello(request):
+    return HttpResponse("<h1>Hello World</h1>")
+
+def about(request):
+    return HttpResponse("<h2>Nosotros</h2>")
