720f3d1d83e6ef040640ae6ea52ea6eb0f7887bd
wsolano <wsolano@ltechec.com>
2023-10-13 16:25:33 -0500

    Creamos dos path y recibir parametros desde URL

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.hello),
-    path('about/', views.about)
+    path('', views.index),
+    path('about/', views.about),
+    path('hello/<str:username>', views.hello),
+    path('project/', views.project),
+    path('task/<int:id>', views.task)
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-def hello(request):
-    return HttpResponse("<h1>Hello World</h1>")
+from django.http import HttpResponse, JsonResponse
+from .models import Project, Task
+from django.shortcuts import get_object_or_404
+
+def index(request):
+    return HttpResponse("<h1>index Page</h1>")
+
+def hello(request, username):
+    return HttpResponse(f"<h1>Hello {username}</h1>")
+
 
 def about(request):
     return HttpResponse("<h2>Nosotros</h2>")
+
+def project(request):
+    projects = list(Project.objects.values())
+    return JsonResponse(projects, safe=False)
+
+def task(request, id):
+    task = get_object_or_404(Task, id=id)
+    return HttpResponse(f"task: {task.tittle}")
